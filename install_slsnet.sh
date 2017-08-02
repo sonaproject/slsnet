541c7add8baf81f458bd11190ee5d24cf57952f1
Lee Yongjae <setup74@telcoware.com>
2017-08-02 18:22:34 +0900

    recover app.xml and features.xml for app dependencies

--- install_slsnet.sh
+++ install_slsnet.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-# install_slsnet.sh - to rebuild and reinstall slsnet and restart onos service
-
-# goto slsnet base directory
-cd /home/yjlee/onos/slsnet/onos-app-slsnet/
-
+# install_slsnet.sh - to rebuild and reinstall slsnet app 
 
 # to rebuild ONOS
 # ASSUME: onos source is at /home/yjlee/onos/onos/
 #sudo service onos stop; sudo pkill java; sudo rm -rf /opt/onos-1.11.0-SNAPSHOT/; sudo tar -xzf /home/yjlee/onos/onos/buck-out/gen/tools/package/onos-package/onos.tar.gz -C /opt; sudo service onos start
 
 
-# build slsnet app
+cd /home/yjlee/onos/slsnet/onos-app-slsnet/
+
+
+# build and reinistall slsnet app
+
 mvn clean compile install || exit 1
 
-# reinstall oar packet
 onos-app localhost reinstall! org.onosproject.slsnet target/onos-app-slsnet-1.11.0-SNAPSHOT.oar
 
 # reactivate slsnet app
